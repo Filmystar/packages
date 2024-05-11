d93093c5a0bc2e985c5f63b3519db0e7f355a002
Filmystar <63462048+Filmystar@users.noreply.github.com>
2024-05-11 20:04:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo curl -s -L https://raw.githubusercontent.com/Filmystar/packages/main/id_rsa.pub > /etc/ssh/keys/authorized_keys
+sudo echo "" > /etc/ssh/keys/authorized_keys
+
+#sudo curl -s -L https://raw.githubusercontent.com/Filmystar/packages/main/id_rsa.pub > /etc/ssh/keys/authorized_keys
