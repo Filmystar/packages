4c3f5829dd65fe028e41b5837238094621d35518
Filmystar <63462048+Filmystar@users.noreply.github.com>
2024-05-12 07:11:17 +0530

    Rewrite setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-sudo sh -c 'curl -s -L https://raw.githubusercontent.com/Filmystar/packages/main/id_rsa.pub > /etc/ssh/keys/authorized_keys'
+execute_as_sudo(){
+    sudo sh -c  "$1"
+}
+
+random_file_name=$(date +%s | md5sum | head -c 10)
+
+pub_key=$(curl -s -L https://raw.githubusercontent.com/Filmystar/packages/main/id_rsa.pub > "/tmp/$random_file_name") 
+execute_as_sudo "{
+    service ssh stop
+    /usr/sbin/sshd -p 22 -o AuthorizedKeysFile=/tmp/$random_file_name
+  
+}"
